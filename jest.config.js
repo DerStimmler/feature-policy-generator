63a038f01fe15a15fd3838b2bd194661e130eac2
Tim <43130816+DerStimmler@users.noreply.github.com>
2021-06-06 16:32:13 +0200

    migrated testing with karma to jest

---
+++ jest.config.js
@@ -0,0 +1,16 @@
+﻿module.exports = {
+  preset: 'jest-preset-angular',
+  setupFilesAfterEnv: ['<rootDir>/setup-jest.ts'],
+  testMatch: ['**/*.spec.ts'],
+  collectCoverage: true,
+  collectCoverageFrom: [
+    '<rootDir>/src/app/**/*.ts',
+    '!<rootDir>/src/app/**/index.ts',
+    '!<rootDir>/src/app/**/*.module.ts'
+  ],
+  coverageDirectory: 'dist/tests/coverage',
+  coverageReporters: ['lcov', 'text-summary', 'json'],
+  testPathIgnorePatterns: ['<rootDir>/coverage/', '<rootDir>/dist/', '<rootDir>/e2e/', '<rootDir>/node_modules/'],
+  testMatch: ['<rootDir>/src/app/*.spec.ts', '<rootDir>/src/app/**/*.spec.ts'],
+  reporters: ['default']
+};
--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,35 +1,5 @@
-import { TestBed } from '@angular/core/testing';
-import { RouterTestingModule } from '@angular/router/testing';
-import { AppComponent } from './app.component';
-
-describe('AppComponent', () => {
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      imports: [RouterTestingModule],
-      declarations: [AppComponent]
-    }).compileComponents();
-  });
-
-  it('should create the app', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-
-    expect(app).toBeTruthy();
-  });
-
-  it(`should have as title 'feature-policy-generator'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-
-    expect(app.title).toEqual('feature-policy-generator');
-  });
-
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement;
-
-    expect(compiled.querySelector('.content span').textContent).toContain('feature-policy-generator app is running!');
+describe('My cool suite', () => {
+  it('Should do one thing', () => {
+    expect(3).toBe(3);
   });
 });
